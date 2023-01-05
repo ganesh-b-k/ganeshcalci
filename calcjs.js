1c236cbb170f0bc1eed73f7d6bc427bb16a3211e
ganesh-b-k <121806143+ganesh-b-k@users.noreply.github.com>
2023-01-05 19:47:28 +0530

    Add files via upload

---
+++ calcjs.js
@@ -0,0 +1,24 @@
+function evaluateMe()
+{
+    var s=document.getElementById('result').value;
+    if(s.length==0)
+        alert("Please enter an Expression")
+        return;
+    try{
+        var res=eval(s);
+        document.getElementById('result').value=res;
+    }
+    catch{
+        alert("invalid expression");
+    }
+}
+function square()
+{
+    var s=document.getElementById('result').value;
+    document.getElementById('result').value=Math.sqrt(s);
+}function cuberoot()
+{
+    var s=document.getElementById('result').value;
+    document.getElementById('result').value=Math.cbrt(s);
+}
+
